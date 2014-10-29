dad93b2f94937ebe6b0ac18f20a4c85e1f807195
Islam Mustafa <islam@integreight.com>
2014-10-29 16:31:18 +0200

    Made final general refactoring

--- 1Sheeld Bootloader/bootldr.c
+++ 1Sheeld Bootloader/bootldr.c
@@ -1,331 +1,260 @@
 /*
 
-  Project:       1Sheeld Bootloader
-  
-  File:          bootldr.c
-                
-  Compiler:      avr-gcc 3.4.2
+	Project:       1Sheeld Bootloader
 
-  Author:        Integreight based on AVR Universal Bootloader by Shao ziyang: http://sourceforge.net/projects/avrub
-                 
-  Date:          2014.5
+	File:          bootldr.c
+
+	Compiler:      avr-gcc 3.4.2
+
+	Author:        Shao ziyang (http://sourceforge.net/projects/avrub) with a little bit of modifications by Integreight, Inc. team.
+
+	Date:          2014.5
 
 */
-//#include "bootcfg.h"
+
 #include "bootldr.h"
-#include <util/setbaud.h>
 
 //user's application start address
 #define PROG_START         0x0000
 
-//receive buffer' size will not smaller than SPM_PAGESIZE
-//#if (BUFFERSIZE < SPM_PAGESIZE)
-//#define BUFSIZE SPM_PAGESIZE
-//#else
-//#define BUFSIZE BUFFERSIZE
-//#endif
-
 //define receive buffer
 unsigned char buf[BUFFERSIZE];
-
-//#if (BUFFERSIZE > 255)
-//unsigned int bufptr, pagptr;
-//#else
 unsigned char bufptr, pagptr;
-//#endif
-
 unsigned char ch, cl, RecivedPacketNo, PacketNoComplement;
-
-//Flash address
-//#if (FLASHEND > 0xFFFFUL)
-//unsigned long int FlashAddr;
-//#else
 unsigned int FlashAddr;
-//#endif
 
 //write one Flash page
 void writeOneFlashPage(unsigned char *buf)
 {
-  boot_page_erase(FlashAddr);                  //erase one Flash page
-  boot_spm_busy_wait();
-  for(pagptr = 0; pagptr < SPM_PAGESIZE; pagptr += 2) //fill data to Flash buffer
-  {
-    boot_page_fill(pagptr, buf[pagptr] + (buf[pagptr + 1] << 8));
-  }
-  boot_page_write(FlashAddr);                  //write buffer to one Flash page
-  boot_spm_busy_wait();                        //wait Flash page write finish
+	boot_page_erase(FlashAddr);                  //erase one Flash page
+	boot_spm_busy_wait();
+	for(pagptr = 0; pagptr < SPM_PAGESIZE; pagptr += 2) //fill data to Flash buffer
+	{
+		boot_page_fill(pagptr, buf[pagptr] + (buf[pagptr + 1] << 8));
+	}
+	boot_page_write(FlashAddr);                  //write buffer to one Flash page
+	boot_spm_busy_wait();                        //wait Flash page write finish
 }
 
 //jump to user's application
 void quitToUserApplication()
 {
-  TCCR1B = 0;
-  boot_rww_enable();                           //enable application section
-  (*((void(*)(void))PROG_START))();            //jump
+	TCCR1B = 0;
+	boot_rww_enable();                           //enable application section
+	(*((void(*)(void))PROG_START))();            //jump
 }
 
 //send a byte to comport
 void sendByte(unsigned char dat)
 {
-  UDRREG(COMPORTNO) = dat;
-  //wait send finish
-  while(!(UCSRAREG(COMPORTNO) & (1<<TXCBIT(COMPORTNO))));
-  UCSRAREG(COMPORTNO) |= (1 << TXCBIT(COMPORTNO));
+	UDR0 = dat;
+	//wait send finish
+	while(!(UCSR0A & (1<<TXC0)));
+	UCSR0A |= (1 << TXC0);
 }
 
 //wait receive a data from comport
 unsigned char readUARTData()
 {
-  while(!dataInCom());
-  return readCom();
+	while(!dataInCom());
+	return readCom();
 }
 
 //calculate CRC checksum
 void calculateCRC(unsigned char *buf)
 {
-//#if (BUFFERSIZE > 255)
-  //unsigned int j;
-//#else
-  unsigned char j;
-//#endif
-  unsigned char i;
-  unsigned int t;
-  unsigned int crc;
-  crc = 0;
-  for(j = BUFFERSIZE; j > 0; j--)
-  {
-    //CRC1021 checksum
-    crc = (crc ^ (((unsigned int) *buf) << 8));
-    for(i = 8; i > 0; i--)
-    {
-      t = crc << 1;
-      if(crc & 0x8000)
-        t = t ^ 0x1021;
-      crc = t;
-    }
-    buf++;
-  }
-  ch = crc / 256;
-  cl = crc % 256;
+	unsigned char j;
+	unsigned char i;
+	unsigned int t;
+	unsigned int crc;
+	crc = 0;
+	for(j = BUFFERSIZE; j > 0; j--)
+	{
+		//CRC1021 checksum
+		crc = (crc ^ (((unsigned int) *buf) << 8));
+		for(i = 8; i > 0; i--)
+		{
+			t = crc << 1;
+			if(crc & 0x8000)
+			t = t ^ 0x1021;
+			crc = t;
+		}
+		buf++;
+	}
+	ch = crc / 256;
+	cl = crc % 256;
 }
 
 
 //Main routine
 int main(void)
 {
-  unsigned char cnt;
-  unsigned char packNO;
-  makeSPIPinsOutput();
-	    unsigned char crch, crcl; 
-//#if (INITUARTDELAY > 0)
-//#if (INITUARTDELAY > 255)
-  //unsigned int di;
-//#else
-  //unsigned char di;
-//#endif
-//#endif
+	unsigned char cnt;
+	unsigned char packNO;
+	unsigned char crch, crcl;
+	unsigned char li;
+	makeSPIPinsOutput();
+	//disable global interrupts
+	cli();
+	//disable watchdog
+	MCUCSR = 0;
+	wdt_disable();
+	//initialize timer1, CTC mode
+	timerInit();
+	//initialize commport with special config value
+	initUART();
+	//comport launch boot
+	cnt = TIMEOUTCOUNT;
+	cl = 0;
+	//Send NAK byte
+	sendByte(XMODEM_NAK);
+	while(1)
+	{
+		if(TIFR & (1<<OCF1A))    //T1 overflow
+		{
+			TIFR |= (1 << OCF1A);
 
-//#if (BUFFERSIZE > 255)
-  //unsigned int li;
-//#else
-  unsigned char li;
-//#endif
+			if(cl == CONNECTCNT)      //determine Connect Key
+			break;
 
-  //disable global interrupts
-  cli();
-  //disable watchdog
-  MCUCSR = 0;
-  wdt_disable();
-  //initialize timer1, CTC mode
-  timerInit();
-  //initialize commport with special config value
-  initUART();  
-//#if (INITUARTDELAY > 0)
-  ////some kind of avr mcu need special delay after comport initialization
-  //for(di = INITUARTDELAY; di > 0; di--)
-    //__asm__ __volatile__ ("nop": : );
-//#endif
-  //comport launch boot
-  cnt = TIMEOUTCOUNT;
-  cl = 0;
-  //Send NAK byte 
-  sendByte(XMODEM_NAK);
-  while(1)
-  {
-    if(TIFRREG & (1<<OCF1A))    //T1 overflow
-    {
-      TIFRREG |= (1 << OCF1A);
+			cnt--;
+			if(cnt == 0)              //connect timeout
+			{
+				quitToUserApplication();                 //quit bootloader
+			}
+		}
 
-      if(cl == CONNECTCNT)      //determine Connect Key
-        break;
+		if(dataInCom())             //receive connect key
+		{
+			if(readCom() == KEY[cl])  //compare ConnectKey
+			cl++;
+			else
+			cl = 0;
+		}
+	}
+	//every interval send a "C",waiting XMODEM control command <soh>
+	cnt = TIMEOUTCOUNTC;
+	while(1)
+	{
+		if(TIFR & (1 << OCF1A))  //T1 overflow
+		{
+			TIFR |= (1 << OCF1A);
+			sendByte(XMODEM_RWC) ;    //send "C"
+			cnt--;
+			if(cnt == 0)              //timeout
+			{
+				quitToUserApplication();                 //quit bootloader
+			}
+		}
+		if(dataInCom())
+		{
+			//if(ReadCom() == XMODEM_SOH)  //XMODEM command <soh>
+			break;
+		}
+	}
 
-      cnt--;
-      if(cnt == 0)              //connect timeout
-      {
-        quitToUserApplication();                 //quit bootloader
-      }
-    }
+	TCCR1B = 0;                   //close timer1
 
-    if(dataInCom())             //receive connect key
-    {
-      if(readCom() == KEY[cl])  //compare ConnectKey
-        cl++;
-      else
-        cl = 0;
-    }
-  }
-  //every interval send a "C",waiting XMODEM control command <soh>
-  cnt = TIMEOUTCOUNTC;
-  while(1)
-  {
-    if(TIFRREG & (1 << OCF1A))  //T1 overflow
-    {
-      TIFRREG |= (1 << OCF1A);
-      sendByte(XMODEM_RWC) ;    //send "C"
-      cnt--;
-      if(cnt == 0)              //timeout
-      {
-        quitToUserApplication();                 //quit bootloader
-      }
-    }
-    if(dataInCom())
-    {
-      //if(ReadCom() == XMODEM_SOH)  //XMODEM command <soh>
-        break;
-    }
-  }
-
-  TCCR1B = 0;                   //close timer1
-
-  //begin to receive data
-  packNO    = 0;
-  bufptr    = 0;
-  cnt       = 0;
-  FlashAddr = 0;
-  while(readUARTData() != XMODEM_EOT)
-  {  
-    packNO++;
-    RecivedPacketNo    =  readUARTData();           //get package number
-    PacketNoComplement = ~readUARTData();
-    for(li = 0; li < BUFFERSIZE; li++)      //receive a full data frame
-    {
-	    buf[bufptr] = readUARTData();
-	    bufptr++;
-    }
-    crch = readUARTData();                       //get CRC
-    crcl = readUARTData();
-	if ((packNO == RecivedPacketNo) && (packNO == PacketNoComplement)) 
-    {
-	  calculateCRC(&buf[bufptr - BUFFERSIZE]);       //calculate checksum
-      if((crch == ch) && (crcl == cl))
-	  {
-//#if BOOTSTARTADDRESS
-  		if(FlashAddr < BOOTSTARTADDRESS)             //avoid write to boot section
-        {
-//#endif
-
-//#if (BUFFERSIZE <= SPM_PAGESIZE)
-          
-          if(bufptr >= SPM_PAGESIZE)          //Flash page full, write flash page;otherwise receive next frame
-          {                                   //receive multi frames, write one page
-            writeOneFlashPage(buf);              //write data to Flash
-            FlashAddr += SPM_PAGESIZE;        //modify Flash page address
-            bufptr = 0;
-          }
-//#else
-          //while(bufptr > 0)                   //receive one frame, write multi pages
-          //{
-            //writeOneFlashPage(&buf[BUFSIZE - bufptr]);
-            //FlashAddr += SPM_PAGESIZE;        //modify Flash page address
-            //bufptr -= SPM_PAGESIZE;
-          //}
-//#endif
-
-//#if BOOTSTARTADDRESS
-        }
-        else                                  //ignore flash write when Flash address exceed BootStart
-        {
-          bufptr = 0;                         //reset receive pointer
-        }
-//#endif
-//read flash, and compare with buffer's content
-//#if (ChipCheck > 0) && (BOOTSTARTADDRESS > 0)
-//#if (BUFFERSIZE < SPM_PAGESIZE)
-        //if((bufptr == 0) && (FlashAddr < BOOTSTARTADDRESS))
-//#else
-        if(FlashAddr < BOOTSTARTADDRESS)
-//#endif
-        {
-          boot_rww_enable();                  //enable application section
-          cl = 1;                             //clear error flag
-          for(pagptr = 0; pagptr < BUFFERSIZE; pagptr++)
-          {
-//#if (FLASHEND > 0xFFFFUL)
-            //if(pgm_read_byte_far(FlashAddr - BUFSIZE + pagptr) != buf[pagptr])
-//#else
-            if(pgm_read_byte(FlashAddr - BUFFERSIZE + pagptr) != buf[pagptr])
-//#endif
-            {
-              cl = 0;                         //set error flag
-              break;
-            }
-          }
-          if(cl)                              //checksum equal, send ACK
-          {
-            sendByte(XMODEM_ACK);
-            cnt = 0;
-          }
-          else
-          {
-            sendByte(XMODEM_NAK);             //checksum error, ask resend
-            cnt++;                            //increase error counter
+	//begin to receive data
+	packNO    = 0;
+	bufptr    = 0;
+	cnt       = 0;
+	FlashAddr = 0;
+	while(readUARTData() != XMODEM_EOT)
+	{
+		packNO++;
+		RecivedPacketNo    =  readUARTData();           //get package number
+		PacketNoComplement = ~readUARTData();
+		for(li = 0; li < BUFFERSIZE; li++)      //receive a full data frame
+		{
+			buf[bufptr] = readUARTData();
+			bufptr++;
+		}
+		crch = readUARTData();                       //get CRC
+		crcl = readUARTData();
+		if ((packNO == RecivedPacketNo) && (packNO == PacketNoComplement))
+		{
+			calculateCRC(&buf[bufptr - BUFFERSIZE]);       //calculate checksum
+			if((crch == ch) && (crcl == cl))
+			{
+				if(FlashAddr < BOOTSTARTADDRESS)             //avoid write to boot section
+				{
+					if(bufptr >= SPM_PAGESIZE)          //Flash page full, write flash page;otherwise receive next frame
+					{                                   //receive multi frames, write one page
+						writeOneFlashPage(buf);              //write data to Flash
+						FlashAddr += SPM_PAGESIZE;        //modify Flash page address
+						bufptr = 0;
+					}
+				}
+				else                                  //ignore flash write when Flash address exceed BootStart
+				{
+					bufptr = 0;                         //reset receive pointer
+				}
+				//read flash, and compare with buffer's content
+				if(FlashAddr < BOOTSTARTADDRESS)
+				{
+					boot_rww_enable();                  //enable application section
+					cl = 1;                             //clear error flag
+					for(pagptr = 0; pagptr < BUFFERSIZE; pagptr++)
+					{
+						if(pgm_read_byte(FlashAddr - BUFFERSIZE + pagptr) != buf[pagptr])
+						{
+							cl = 0;                         //set error flag
+							break;
+						}
+					}
+					if(cl)                              //checksum equal, send ACK
+					{
+						sendByte(XMODEM_ACK);
+						cnt = 0;
+					}
+					else
+					{
+						sendByte(XMODEM_NAK);             //checksum error, ask resend
+						cnt++;                            //increase error counter
+						packNO--;
+						bufptr = 0;
+						FlashAddr -= BUFFERSIZE;             //modify Flash page address
+					}
+				}
+				else                                  //don't need verify, send ACK directly
+				{
+					sendByte(XMODEM_ACK);
+					cnt = 0;
+				}
+			}
+			else                                    //CRC
+			{
+				packNO--;
+				bufptr = 0;
+				sendByte(XMODEM_NAK);                 //require resend
+				cnt++;
+			}
+		}
+		else //PackNo
+		{
 			packNO--;
-			bufptr = 0;
-            FlashAddr -= BUFFERSIZE;             //modify Flash page address
-          }
-        }
-        else                                  //don't need verify, send ACK directly
-        {
-          sendByte(XMODEM_ACK);
-          cnt = 0;
-        }
-//#else
-        //sendByte(XMODEM_ACK);                 //no verify, send ACK directly
-        //cnt = 0;
-//#endif
-      }
-      else                                    //CRC sdfsdfsdf
-      {
-		packNO--;
-		bufptr = 0;
-        sendByte(XMODEM_NAK);                 //require resend
-        cnt++;
-      }
-    }
-    else //PackNo
-    {    
-      packNO--;
-	  bufptr = 0;                               //reinitialize the pointer 
-      cnt++;
-	  sendByte(XMODEM_NAK);                    //require resend
-    }
+			bufptr = 0;                               //reinitialize the pointer
+			cnt++;
+			sendByte(XMODEM_NAK);                    //require resend
+		}
 
-    if(cnt > 3)                               //too many error, abort update
-    {
-        break;
-    }  
-      
-  }
-  if(cnt == 0)
-  {
-	   sendByte(XMODEM_ACK);  
-  }
-  else
-  {
-	   sendByte(XMODEM_CAN);
-  }
-  quitToUserApplication();                                     //quit bootloader
-  return 0;
+		if(cnt > 3)                               //too many error, abort update
+		{
+			break;
+		}
+		
+	}
+	if(cnt == 0)
+	{
+		sendByte(XMODEM_ACK);
+	}
+	else
+	{
+		sendByte(XMODEM_CAN);
+	}
+	quitToUserApplication();                                     //quit bootloader
+	return 0;
 }
 
 //End of file: bootldr.c
--- 1Sheeld Bootloader/bootldr.h
+++ 1Sheeld Bootloader/bootldr.h
@@ -1,19 +1,19 @@
 /*
 
-  Project:       1Sheeld Bootloader
-  
-  File:          bootldr.h
-                
-  Compiler:      avr-gcc 3.4.2
+	Project:       1Sheeld Bootloader
 
-  Author:        Integreight based on AVR Universal Bootloader by Shao ziyang: http://sourceforge.net/projects/avrub
-                 
-  Date:          2014.5
+	File:          bootldr.h
+
+	Compiler:      avr-gcc 3.4.2
+
+	Author:        Shao ziyang (http://sourceforge.net/projects/avrub) with a little bit of modifications by Integreight, Inc. team.
+
+	Date:          2014.5
 
 */
 
 #ifndef _BOOTLDR_H_
-#define _BOOTLDR_H_        1
+#define _BOOTLDR_H_
 
 #include <avr/io.h>
 #include <avr/wdt.h>
@@ -21,188 +21,76 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 
+//Define UART buffer's length
+#define BUFFERSIZE			128
 
-//define uart buffer's length
-#define BUFFERSIZE         128
+//Define system clock
+#define F_CPU				7372800UL
 
-//system clock(Hz)
-#ifndef F_CPU
-#define F_CPU              7372800UL
-#endif
-
-//baudrate
-#define BAUDRATE           115200
+//Define communication's baudrate
+#define BAUDRATE			115200
 
 //Boot section start address(byte)
-//define BootStart to 0 will disable this function
-#define BOOTSTARTADDRESS          0x1C00 * 2
+#define BOOTSTARTADDRESS	0x1C00 * 2
 
-//max wait password time = TimeOutCnt * timeclk
-//timeout count
-#define TIMEOUTCOUNT         1
+//Basic timer interval in ms
+#define TIMERINTERVAL		200
 
-//basic timer interval(ms)
-#define TIMERINTERVAL        200
+//Define the password waiting timout
+//(Maximum password waiting time = TIMEOUTCOUNT * TIMERINTERVAL)
+#define TIMEOUTCOUNT		1
 
-//max wait data time = TimeOutCntC * timeclk
-//send 'C' command count
-#define TIMEOUTCOUNTC        250
+//Define the data waiting timout
+//(Maximum data waiting time = TIMEOUTCOUNTC * TIMERINTERVAL)
+#define TIMEOUTCOUNTC		250
 
-//password length
-#define CONNECTCNT         8
+//Password character length
+#define CONNECTCNT			8
 
-//password
-//#if LEVELMODE == 0
+//The password
 unsigned char KEY[] = {0x64, 0x0E, 0x1C, 0x39, 0x14, 0x28, 0x57, 0xAA};
-//#endif
 
-//comport number: 0/1/2..
-#define COMPORTNO          1
-
-///////////////////////////////////////////////////////////////
-//Don't modify code below, unless your really konw what to do//
-///////////////////////////////////////////////////////////////
-
-//certain version compiler missing this in head files
-//#ifndef SPM_PAGESIZE
-//#error "Not define SPM_PAGESIZE, please define below or update your WinAVR"
-////#define SPM_PAGESIZE       XXX
-//#endif
-
-////certain version compiler missing this in head files
-//#ifndef FLASHEND
-//#error "Not define FLASHEND, please define below or update your WinAVR"
-////#define FLASHEND           XXX
-//#endif
-
-//two buffer size must be multiple or submultiple relation
-//#if BUFFERSIZE >= SPM_PAGESIZE
-//#if (BUFFERSIZE / SPM_PAGESIZE * SPM_PAGESIZE) != BUFFERSIZE
-//#error "Result of (BUFFERSIZE / SPM_PAGESIZE) is not a Integer!"
-//#error "Please check and set 'BUFFERSIZE/SPM_PAGESIZE' Macro again!"
-//#endif
-//#else
-//#if (SPM_PAGESIZE /BUFFERSIZE * BUFFERSIZE) != SPM_PAGESIZE
-//#error "Result of (BUFFERSIZE / SPM_PAGESIZE) is not a Integer!"
-//#error "Please check and set 'BUFFERSIZE/SPM_PAGESIZE' Macro again!"
-//#endif
-//#endif
-
-#ifndef BAUD
-#define BAUD BAUDRATE
-#endif
-
-//calculate baudrate register
+//Calculate baudrate register value
 #define BAUDREG            ((unsigned int)((F_CPU * 10) / (16UL * BAUDRATE) - 5) / 10)
 
-//check baudrate register error
-//mocro below maybe not same in different C compiler
-#define freqTemp           (16UL * BAUDRATE * (((F_CPU * 10) / (16 * BAUDRATE) + 5)/ 10))
-//#if ((FreqTemp * 50) > (51 * F_CPU)) || ((FreqTemp * 50) < (49 * F_CPU))
-//#error "BaudRate error > 2% ! Please check BaudRate and F_CPU value."
-//#endif
-
-//#ifndef F_CPU
-//#define F_CPU 7372800UL
-//#endif
-
-//internal use macro
-#define CONCAT(a, b)       a ## b
-#define CONCAT3(a, b, c)   a ## b ## c
-
-//register of PORT and bit define
-#define PORTREG(No)        CONCAT(PORT, No)
-#define PINREG(No)         CONCAT(PIN, No)
-#define UDRREG(No)         CONCAT(UDR, No)
-#define DDRREG(No)         CONCAT(DDR, No)
-#define TXCBIT(No)         CONCAT(TXC, No)
-#define RXCBIT(No)         CONCAT(RXC, No)
-#define RXENBIT(No)        CONCAT(RXEN, No)
-#define TXENBIT(No)        CONCAT(TXEN, No)
-#define URSELBIT(No)       CONCAT(URSEL, No)
-
-//comport register
-#define UBRRHREG(No)       CONCAT3(UBRR, No, H)
-#define UBRRLREG(No)       CONCAT3(UBRR, No, L)
-#define UCSRAREG(No)       CONCAT3(UCSR, No, A)
-#define UCSRBREG(No)       CONCAT3(UCSR, No, B)
-#define UCSRCREG(No)       CONCAT3(UCSR, No, C)
-#define UCSZBIT(No1, No2)  CONCAT3(UCSZ, No1, No2)
-
-//some kind of AVR need URSEL define when comport initialize
-#if defined(URSEL) || defined(URSEL0)
-#define USEURSEL           (1 << URSELBIT(COMPORTNo))
-#else
-#define USEURSEL           0
-#endif
-
-//define UART0 register
-#if !defined(UDR0)
-#define UBRR0H             UBRRH
-#define UBRR0L             UBRRL
-#define UCSR0A             UCSRA
-#define UCSR0B             UCSRB
-#define UCSR0C             UCSRC
-#define UDR0               UDR
-#define TXC0               TXC
-#define RXC0               RXC
-#define RXEN0              RXEN
-#define TXEN0              TXEN
-#define UCSZ01             UCSZ1
-#define UCSZ00             UCSZ0
-#define URSEL0             URSEL
-#endif
-
-
-//timer1: prescale 1024, CTC mode 4, interval unit is millisecond
+//Timer1 configuration: (prescaler: 1024, CTC mode: 4 and interval unit is millisecond)
 #define timerInit()															\
 {																			\
 	OCR1A  = (unsigned int)(TIMERINTERVAL * (F_CPU  / (1024 * 1000.0f)));	\
-    TCCR1A = 0;																\
-    TCCR1B = (1 << WGM12)|(1 << CS12)|(1 << CS10);							\
+	TCCR1A = 0;																\
+	TCCR1B = (1 << WGM12)|(1 << CS12)|(1 << CS10);							\
 }
 
-// make spi pins output
+//Configure SPI pins as output as a precaution
 #define makeSPIPinsOutput()						\
 {												\
 	DDRB|=(1<<DDB5)|(1<<DDB6)|(1<<DDB7);		\
 }
 
-//timer1 overflow register
-#ifdef TIFR
-#define TIFRREG            TIFR
-#else
-#define TIFRREG            TIFR1
-#endif
+//Uart's UBBR value
+#define UBRR_BOOT_VALUE 3
 
-#define UBRR_BOOT_VALUE 3 
-
-//Initialize USART
+//Initialize Uart
 #define initUART()												\
 {                                                               \
 	UBRR0H = (UBRR_BOOT_VALUE>>8);                              \
 	UBRR0L = UBRR_BOOT_VALUE;                                   \
 	UCSR0C = (1<<URSEL0) | (1 << UCSZ00)|(1 << UCSZ01);			\
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);						\
-	UCSR0A = 2;													\
 	UBRR1H = (UBRR_BOOT_VALUE>>8);								\
 	UBRR1L = UBRR_BOOT_VALUE;                                   \
 	UCSR1C = (1<<URSEL1) | (1 << UCSZ10)|(1 << UCSZ11);			\
 	UCSR1B = (1 << RXEN1) | (1 << TXEN1);						\
 }
 
-//Xmodem control commands
-#define XMODEM_NUL         0x00
-#define XMODEM_SOH         0x01
-#define XMODEM_STX         0x02
+//Used X-modem control commands
 #define XMODEM_EOT         0x04
 #define XMODEM_ACK         0x06
 #define XMODEM_NAK         0x15
 #define XMODEM_CAN         0x18
-#define XMODEM_EOF         0x1A
 #define XMODEM_RWC         'C'
-#define dataInCom()        (UCSRAREG(COMPORTNO) & (1 << RXCBIT(COMPORTNO)))
-#define readCom()          UDRREG(COMPORTNO)
+#define dataInCom()        (UCSR0A & (1 << RXC0))
+#define readCom()          UDR0
 
 #endif
 
